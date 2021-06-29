b2b6e7ce776dee9501a74e8a033e000158f7e313
fedecech <federicocech@gmail.com>
2021-06-30 01:02:28 +0200

    fix next_clear_step fn

--- src/core/util.ts
+++ src/core/util.ts
@@ -1,3 +1,6 @@
+import { AStarFinder } from 'src/finders/astar-finder';
+import { Coordinates } from 'src/interfaces/gameData';
+import { PossibleMoves } from 'src/routes/move';
 import { Node } from './Node';
 
 /**
@@ -34,3 +37,85 @@ export function backtrace(
 
 	return path.reverse();
 }
+/**
+ * Find shortest path from point A to point B using A* algorithm.
+ * @param s [starting cordinates]
+ * @param e [ending coordinates]
+ * @param matrix [2D array fo board]
+ */
+export const next_path = (
+	s: Coordinates,
+	e: Coordinates,
+	matrix: number[][]
+) => {
+	return new AStarFinder({
+		grid: {
+			matrix,
+		},
+		diagonalAllowed: false,
+		includeStartNode: false,
+		includeEndNode: true,
+	}).findPath(s, e);
+};
+
+/**
+ * Finds next possible step where there is no obstacle.
+ * @param head [starting coordinates]
+ * @param walls [obstacles coordinates]
+ * @param width [width of board]
+ * @param height [height of board]
+ */
+const next_clear_step = (
+	head: Coordinates,
+	walls: Coordinates[],
+	width: number,
+	height: number
+) => {
+	const up = { x: head.x - 1, y: head.y, direction: PossibleMoves.UP };
+	const dw = { x: head.x + 1, y: head.y, direction: PossibleMoves.DOWN };
+	const lt = { x: head.x, y: head.y - 1, direction: PossibleMoves.LEFT };
+	const rt = { x: head.x - 1, y: head.y + 1, direction: PossibleMoves.RIGHT };
+	let dirs = [up, dw, lt, rt];
+
+	// remove directions were there are external walls
+	dirs = dirs.filter((d) => d.x < width && d.y < height);
+
+	walls.forEach((w) => {
+		if (w.x === up.x && w.y === up.y) {
+			dirs.splice(dirs.indexOf(up));
+		} else if (w.x === dw.x && w.y === dw.y) {
+			dirs.splice(dirs.indexOf(dw));
+		} else if (w.x === lt.x && w.y === lt.y) {
+			dirs.splice(dirs.indexOf(lt));
+		} else if (w.x === rt.x && w.y === rt.y) {
+			dirs.splice(dirs.indexOf(rt));
+		}
+	});
+
+	return dirs.length > 0 ? dirs[0].direction : PossibleMoves.LEFT; // all 4 ways (up, left, right, down) have walls
+};
+
+export const next_move = (
+	path: number[][],
+	head: Coordinates,
+	walls: Coordinates[],
+	width: number,
+	height: number
+) => {
+	if (path.length < 1) {
+		return next_clear_step(head, walls, width, height);
+	}
+	const step = path[0];
+	const x = step[0];
+	const y = step[1];
+
+	if (x < head.x) {
+		return PossibleMoves.LEFT;
+	} else if (x > head.x) {
+		return PossibleMoves.RIGHT;
+	} else if (y > head.y) {
+		return PossibleMoves.DOWN;
+	} else {
+		return PossibleMoves.UP;
+	}
+};
--- src/routes/move.ts
+++ src/routes/move.ts
@@ -1,84 +1,18 @@
-// import { AStarFinder } from './../finders/astar-finder';
 import { Heuristic } from '../core/Heuristic';
 import { Coordinates } from '../interfaces/gameData';
 import { Router, Request, Response } from 'express';
 import { GameData } from '../interfaces/gameData';
 import { Converter } from '../core/Converter';
-import { AStarFinder } from '../finders/astar-finder';
+import { next_move, next_path } from 'src/core/util';
 
 const router = Router();
 
-enum PossibleMoves {
+export enum PossibleMoves {
 	UP = 'up',
 	DOWN = 'down',
 	LEFT = 'left',
 	RIGHT = 'right',
 }
-
-const next_path = (s: Coordinates, e: Coordinates, matrix: number[][]) => {
-	return new AStarFinder({
-		grid: {
-			matrix,
-		},
-		diagonalAllowed: false,
-		includeStartNode: false,
-		includeEndNode: true,
-	}).findPath(s, e);
-};
-
-const next_clear_step = (
-	head: Coordinates,
-	walls: Coordinates[],
-	width: number,
-	height: number
-) => {
-	const up = { x: head.x - 1, y: head.y, direction: PossibleMoves.UP };
-	const dw = { x: head.x + 1, y: head.y, direction: PossibleMoves.DOWN };
-	const lt = { x: head.x, y: head.y - 1, direction: PossibleMoves.LEFT };
-	const rt = { x: head.x - 1, y: head.y + 1, direction: PossibleMoves.RIGHT };
-	let dirs = [up, dw, lt, rt];
-
-	// remove directions were there are external walls
-	dirs = dirs.filter((d) => d.x < width - 1 && d.y < height - 1);
-
-	walls.forEach((w) => {
-		if (w.x === up.x && w.y === up.y) {
-			dirs.splice(dirs.indexOf(up));
-		} else if (w.x === dw.x && w.y === dw.y) {
-			dirs.splice(dirs.indexOf(dw));
-		} else if (w.x === lt.x && w.y === lt.y) {
-			dirs.splice(dirs.indexOf(lt));
-		} else if (w.x === rt.x && w.y === rt.y) {
-			dirs.splice(dirs.indexOf(rt));
-		}
-	});
-
-	return dirs.length > 0 ? dirs[0].direction : PossibleMoves.LEFT; // all 4 ways (up, left, right, down) have walls
-};
-const next_move = (
-	path: number[][],
-	head: Coordinates,
-	walls: Coordinates[],
-	width: number,
-	height: number
-) => {
-	if (path.length < 1) {
-		return next_clear_step(head, walls, width, height);
-	}
-	const step = path[0];
-	const x = step[0];
-	const y = step[1];
-
-	if (x < head.x) {
-		return PossibleMoves.LEFT;
-	} else if (x > head.x) {
-		return PossibleMoves.RIGHT;
-	} else if (y > head.y) {
-		return PossibleMoves.DOWN;
-	} else {
-		return PossibleMoves.UP;
-	}
-};
 
 const logs = (gameData: GameData, matrix: number[][], food: Coordinates[]) => {
 	console.log('Game Id: ', gameData.game.id);
@@ -89,8 +23,8 @@ const logs = (gameData: GameData, matrix: number[][], food: Coordinates[]) => {
 
 router.post('/move', (req: Request, res: Response) => {
 	const gameData = req.body as GameData;
-
 	const { width, height } = gameData.board;
+
 	const converter = new Converter(gameData);
 	const matrix = converter.getMatrix();
 	const food = converter.getFood();
